8509e54eac8df0fa044fad9a82b138e70f3b9647
Sanuj <psanuj@vmware.com>
2023-12-08 23:31:05 +0530

    Added flow

---
+++ db.py
@@ -0,0 +1,19 @@
+import firebase_admin
+from firebase_admin import firestore
+
+
+class DBService:
+    def __init__(self):
+        self.app = firebase_admin.initialize_app()
+        self.db = firestore.client()
+
+    def get_all_slots(self) -> (dict,dict):
+        users_ref = self.db.collection("slots")
+        docs = users_ref.stream()
+        slots = dict()
+        reverse_mapping = dict()
+
+        for doc in docs:
+            slots[doc.to_dict().get('id')] = doc.to_dict().get('title')
+            slots[doc.to_dict().get('title')] = doc.to_dict().get('id')
+        return slots, reverse_mapping
--- service.py
+++ service.py
@@ -2,6 +2,7 @@ import calendar
 import json
 import datetime
 
+from db import DBService
 from model.booking import Booking
 from model.enums import InteractiveRequestType, Slot, Month, Screen
 from model.flow import FlowResponse, FlowRequest
@@ -16,6 +17,8 @@ from whatsapp_api import WhatsappApi
 class BoxService:
     def __init__(self):
         self.api_service = WhatsappApi()
+        self.db_service = DBService()
+        self.slots, self.revers_slots_mapping = self.db_service.get_all_slots()
 
     def process_interactive_message(self, mobile: str,
                                     message: InteractiveWebhookMessage):
@@ -209,40 +212,30 @@ class BoxService:
         date = datetime.datetime.fromtimestamp(float(date_selected) / 1000,
                                                tz=datetime.timezone.utc)
         response['selected_date'] = f'{date.strftime("%d %b %Y")}'
-        print(date)
         #  TODO check available slots
-        slots = [{'id': '5', 'title': '5 AM - 6 AM'},
-                 {'id': '6', 'title': '6 AM - 7 AM'},
-                 {'id': '7', 'title': '7 AM - 8 AM'},
-                 {'id': '8', 'title': '8 AM - 9 AM'},
-                 {'id': '9', 'title': '9 AM - 10 AM'},
-                 {'id': '10', 'title': '10 AM - 11 AM'},
-                 {'id': '11', 'title': '11 AM - 12 PM'},
-                 {'id': '12', 'title': '12 PM - 1 PM'},
-                 {'id': '13', 'title': '1 PM - 2 PM'},
-                 {'id': '14', 'title': '2 PM - 3 PM'},
-                 {'id': '15', 'title': '3 PM - 4 PM'},
-                 {'id': '16', 'title': '4 PM - 5 PM'},
-                 {'id': '17', 'title': '5 PM - 6 PM'},
-                 {'id': '18', 'title': '6 PM - 7 PM'},
-                 {'id': '19', 'title': '7 PM - 8 PM'},
-                 {'id': '20', 'title': '8 PM - 9 PM'},
-                 {'id': '21', 'title': '9 PM - 10 PM'},
-                 {'id': '22', 'title': '10 PM - 11 PM'},
-                 {'id': '23', 'title': '11 PM - 12 PM'}]
-        response['slots'] = slots
+        response['slots'] = self.slots
         return response, Screen.SLOT_SELECTION.value
 
     def process_slot_screen_data(self, flow_request):
         date_selected = flow_request.data.get("selected_date")
         date = datetime.datetime.strptime(date_selected, "%d %b %Y")
         slots_selected = flow_request.data.get("slots")
-        print(date)
-        print(slots_selected)
         response = dict()
+        if not slots_selected or len(slots_selected) == 0:
+            response['error_message'] = "Please select at least 1 slot"
+            return response, Screen.SLOT_SELECTION.value
         response['selected_date'] = f"{date_selected}"
         response['slots'] = f"{','.join(slots_selected)}"
         return response, Screen.BOOKING_CONFIRMATION.value
+
+    def process_booking_confirmation_screen_data(self, flow_request):
+        date_selected = flow_request.data.get("selected_date")
+        slots = flow_request.data.get("slots").split(",")
+        print(date_selected, slots)
+        response = dict()
+        response['selected_date'] = f"{date_selected}"
+        response['slots'] = f"{','.join(slots)}"
+        return response, Screen.SUCCESS.value
 
 
 if __name__ == '__main__':
