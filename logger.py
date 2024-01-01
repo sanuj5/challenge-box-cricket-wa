5973e97fc3cd60c50158c712413f08b7de8e3810
Sanuj <psanuj@vmware.com>
2024-01-02 00:40:25 +0530

    Added payment

--- logger.py
+++ logger.py
@@ -1,16 +1,23 @@
 import logging
-logger = logging.getLogger()
+logging.root.handlers = []
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
 
 
 class Logger:
     @staticmethod
     def info(message, *args, **kwargs):
-        logger.info(message, *args, **kwargs)
+        logging.info(message, *args, **kwargs)
 
     @staticmethod
     def debug(message, *args, **kwargs):
-        logger.debug(message, *args, **kwargs)
+        logging.debug(message, *args, **kwargs)
 
     @staticmethod
     def error(message, *args, **kwargs):
-        logger.error(message, *args, **kwargs)
+        logging.error(message, *args, **kwargs)
