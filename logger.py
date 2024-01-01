303d6850fcc16309ee49c516f64ecf1e56fe5348
Sanuj <psanuj@vmware.com>
2024-01-02 00:55:12 +0530

    Added payment

--- logger.py
+++ logger.py
@@ -2,7 +2,6 @@ import logging
 import sys
 
 log = logging.getLogger()
-log.setLevel(logging.DEBUG)
 _format = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
 ch = logging.StreamHandler(sys.stdout)
