9de68e17d548704abee3bcb1c7564335a29a1210
Sanuj <psanuj@vmware.com>
2024-04-18 17:08:13 +0530

    Refactored code

--- external/whatsapp_api.py
+++ external/whatsapp_api.py
@@ -25,7 +25,7 @@ class WhatsappApi:
         headers = self.get_headers(header)
         json_data = json.dumps(data, default=lambda o: o.__dict__)
         Logger.info(json_data)
-        r = requests.post(url=self.url, data=json.loads(json_data), headers=headers)
+        r = requests.post(url=self.url, data=json_data, headers=headers)
         Logger.info(r.content)
 
     def get_headers(self, header):
