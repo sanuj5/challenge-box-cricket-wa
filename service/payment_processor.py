0aa51d5c077caedfc004e43850d32de986c4fb84
Sanuj <psanuj@vmware.com>
2024-04-28 15:26:08 +0530

    Refactored code

--- service/payment_processor.py
+++ service/payment_processor.py
@@ -81,7 +81,9 @@ class PaymentProcessor(BaseProcessor):
             # razorpay_payment_status = self.payment_service.get_payment(
             #     message.payment.reference_id)
             if (
-                    wa_payment_status.get("status") == "CAPTURED"
+                    wa_payment_status.get("payments")
+                    and wa_payment_status.get("payments")[0]
+                    and wa_payment_status.get("payments")[0].get("status") == "CAPTURED"
             ):
                 self.db_service.confirm_booking(existing_booking,
                                                 message.payment.reference_id,
