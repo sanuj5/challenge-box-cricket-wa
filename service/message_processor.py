96ad2cdc7eb2650340b116b070088981175b13f5
Sanuj <psanuj@vmware.com>
2024-06-01 13:58:27 +0530

    Refactored code

--- service/message_processor.py
+++ service/message_processor.py
@@ -201,9 +201,9 @@ class NfmMessageProcessor(BaseMessageProcessor):
             )
             return_message = self.mbs.get_interactive_payment_message_gw(
                 mobile=mobile,
-                payment_amount=total_amount * self.amount_offset,  # TODO
+                payment_amount=total_amount * self.amount_offset,
                 reference_id=token,
-                amount_offset=1,  # TODO
+                amount_offset=100,
                 message_body=f"""
 Date: {date} 
 Slots: {slots_title}
