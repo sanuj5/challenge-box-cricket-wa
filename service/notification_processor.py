10ad5faec59c250d3e6aeb75d781f3cdb7c2ebfc
Sanuj <psanuj@vmware.com>
2024-07-20 13:12:23 +0530

    Added policies

--- service/notification_processor.py
+++ service/notification_processor.py
@@ -58,7 +58,7 @@ class NotificationProcessor(BaseProcessor):
         else:
             final_message = "    --------------------------------------------------------------    ".join(
                 [
-                    f"_*BOOKING {ind + 1}:*_ +{booking.mobile}, {self.db_service.get_user_details(booking.mobile) or ""} --> {',   '.join([self.slots.get(slot).get("title") for slot in booking.slots])}"
+                    f"_*BOOKING {ind + 1}:*_ +{booking.mobile}, {self.db_service.get_user_details(booking.mobile) or ""} --> {',   '.join([self.slots.get(slot).get("title") for slot in booking.slots.sort()])}"
                     for ind, booking in enumerate(bookings)
                 ])
         for mobile_number in mobile_numbers:
